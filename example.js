f4f4fbc2454f8482b6f5bf68dee0b39471899a9e
russEco <russstreet@economist.com>
2015-06-25 11:17:31 +0100

    added tinted panels

--- example.js
+++ example.js
@@ -5,102 +5,108 @@ import TabView from './';
 export default (
   <TabView>
     <div title="Politics">
+    <div className="TabView--Views--Tint"></div>
      <a href="#">
-       <figure>
+       <figure className="TabView--View--Content">
          <img src="http://lorempixel.com/g/300/169/cats"/>
          <figcaption>this is the caption</figcaption>
        </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/people"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/city"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/fashion"/>
           <figcaption>this is the caption</figcaption>
         </figure>
      </a>
     </div>
     <div title="Business & Economics">
+    <div className="TabView--Views--Tint"></div>
      <a href="#">
-       <figure>
+       <figure className="TabView--View--Content">
          <img src="http://lorempixel.com/g/300/169/food"/>
          <figcaption>this is the caption</figcaption>
        </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/nightlife"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/sports"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/nature"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
     </div>
     <div title="Science & technology">
+    <div className="TabView--Views--Tint"></div>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/transport"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/abstract"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/technics"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/cats"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
     </div>
     <div title="History">
+    <div className="TabView--Views--Tint"></div>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/nightlife"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/sports"/>
         <figcaption>this is the caption</figcaption>
       </figure>
      </a>
-      <figure>
+     <a href="#">
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/food"/>
         <figcaption>this is the caption</figcaption>
       </figure>
+     </a>
      <a href="#">
-      <figure>
+      <figure className="TabView--View--Content">
         <img src="http://lorempixel.com/g/300/169/city"/>
         <figcaption>this is the caption</figcaption>
       </figure>
