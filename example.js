cef7ada9bc1a2837789459d6e5e0b30bb1f0d38d
russEco <russstreet@economist.com>
2015-06-25 10:03:38 +0100

    initial commit

---
+++ example.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import TabView from './';
+
+
+export default (
+  <TabView>
+    <div title="Politics">
+     <a href="#">
+       <figure>
+         <img src="http://lorempixel.com/g/300/169/cats"/>
+         <figcaption>this is the caption</figcaption>
+       </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/people"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/city"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/fashion"/>
+          <figcaption>this is the caption</figcaption>
+        </figure>
+     </a>
+    </div>
+    <div title="Business & Economics">
+     <a href="#">
+       <figure>
+         <img src="http://lorempixel.com/g/300/169/food"/>
+         <figcaption>this is the caption</figcaption>
+       </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/nightlife"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/sports"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/nature"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+    </div>
+    <div title="Science & technology">
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/transport"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/abstract"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/technics"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/cats"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+    </div>
+    <div title="History">
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/nightlife"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/sports"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/food"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     <a href="#">
+      <figure>
+        <img src="http://lorempixel.com/g/300/169/city"/>
+        <figcaption>this is the caption</figcaption>
+      </figure>
+     </a>
+    </div>
+  </TabView>
+);
+
