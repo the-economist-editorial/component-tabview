df8a3fa31c7f4226e58ca2f559ebeb7d556fec81
russEco <russstreet@economist.com>
2015-06-26 17:07:27 +0100

    included image caption

--- example.js
+++ example.js
@@ -1,116 +1,79 @@
 import React from 'react';
 import TabView from './';
+import ImageCaption from './../component-imagecaption';
 
+var child = {
+  caption: "First caption",
+  src: "http://lorempixel.com/g/300/169/food",
+  href: "http://www.google.com"
+};
+var child2 = {
+  caption: "First caption",
+  src: "http://lorempixel.com/g/300/169/food",
+  href: "http://www.google.com"
+};
 
 export default (
   <TabView>
     <div title="Politics">
     <div className="TabView--Views--Tint"></div>
-     <a href="#">
-       <figure className="TabView--View--Content">
-         <img src="http://lorempixel.com/g/300/169/cats"/>
-         <figcaption>this is the caption</figcaption>
-       </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/people"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/city"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/fashion"/>
-          <figcaption>this is the caption</figcaption>
-        </figure>
-     </a>
+    <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
     </div>
     <div title="Business & Economics">
     <div className="TabView--Views--Tint"></div>
-     <a href="#">
-       <figure className="TabView--View--Content">
-         <img src="http://lorempixel.com/g/300/169/food"/>
-         <figcaption>this is the caption</figcaption>
-       </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/nightlife"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/sports"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/nature"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
     </div>
     <div title="Science & technology">
     <div className="TabView--Views--Tint"></div>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/transport"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/abstract"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/technics"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/cats"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
     </div>
     <div title="History">
     <div className="TabView--Views--Tint"></div>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/nightlife"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/sports"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/food"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
-     <a href="#">
-      <figure className="TabView--View--Content">
-        <img src="http://lorempixel.com/g/300/169/city"/>
-        <figcaption>this is the caption</figcaption>
-      </figure>
-     </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
+     <a href={child.href}>
+         <ImageCaption caption={child.caption} src={child.src}></ImageCaption>
+    </a>
     </div>
   </TabView>
 );
