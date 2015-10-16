6b254ffa8c9646acd1d610fa1aaebd4942694e3e
umbobabo <umbertobabini@economist.com>
2015-10-16 17:22:32 +0100

    Finalised separation of WIF customisation

--- example.js
+++ example.js
@@ -16,124 +16,206 @@ exports['default'] = _react2['default'].createElement(
   'div',
   null,
   _react2['default'].createElement(
-    _index2['default'],
-    null,
+    'div',
+    { className: 'generic-example' },
     _react2['default'].createElement(
-      'div',
-      { title: 'Politics' },
+      _index2['default'],
+      null,
       _react2['default'].createElement(
-        'a',
-        { href: '#' },
+        'div',
+        { title: 'Politics' },
         _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/cats' }),
+          'a',
+          { href: '#' },
           _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/cats' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/people' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/city' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/fashion' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
           )
         )
       ),
       _react2['default'].createElement(
-        'a',
-        { href: '#' },
+        'div',
+        { title: 'Business & Economics' },
         _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/people' }),
+          'a',
+          { href: '#' },
           _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/food' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nightlife' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/sports' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nature' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
           )
         )
       ),
       _react2['default'].createElement(
-        'a',
-        { href: '#' },
+        'div',
+        { title: 'Science & technology' },
         _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/city' }),
+          'a',
+          { href: '#' },
           _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/transport' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/abstract' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/technics' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
+          )
+        ),
+        _react2['default'].createElement(
+          'a',
+          { href: '#' },
+          _react2['default'].createElement(
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/cats' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
           )
         )
       ),
       _react2['default'].createElement(
-        'a',
-        { href: '#' },
+        'div',
+        { title: 'History' },
         _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/fashion' }),
+          'a',
+          { href: '#' },
           _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
-          )
-        )
-      )
-    ),
-    _react2['default'].createElement(
-      'div',
-      { title: 'Business & Economics' },
-      _react2['default'].createElement(
-        'a',
-        { href: '#' },
-        _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/food' }),
-          _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
-          )
-        )
-      ),
-      _react2['default'].createElement(
-        'a',
-        { href: '#' },
-        _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nightlife' }),
-          _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
-          )
-        )
-      ),
-      _react2['default'].createElement(
-        'a',
-        { href: '#' },
-        _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/sports' }),
-          _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
-          )
-        )
-      ),
-      _react2['default'].createElement(
-        'a',
-        { href: '#' },
-        _react2['default'].createElement(
-          'figure',
-          { className: 'TabView--View--Content' },
-          _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nature' }),
-          _react2['default'].createElement(
-            'figcaption',
-            null,
-            'this is the caption'
+            'figure',
+            { className: 'TabView--View--Content' },
+            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nightlife' }),
+            _react2['default'].createElement(
+              'figcaption',
+              null,
+              'this is the caption'
+            )
           )
         )
       )
