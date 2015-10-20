5d9104733cb78e32e18043e4c17e4082c5e831a3
umbobabo <umbertobabini@economist.com>
2015-10-20 15:31:22 +0100

    Optimised CSS

--- example.es6
+++ example.es6
@@ -5,83 +5,82 @@ export default (
   <div>
     <div className="generic-example">
       <TabView>
-        <div title="Politics">
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/cats"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/people"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/city"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/fashion"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
+        <div title="Tab 1">
+          <img src="http://lorempixel.com/g/400/500/nightlife" style={{ float: 'left' }}/>
+          Lorem ipsum dolor sit amet, an eam error laboramus conceptam. Eu quiså
+          iudicabit philosophia est. Ex noster phaedrum sapientem sed. Tacimates
+          reformidans vis cu, nec delectus suscipiantur id, sit unum regione
+          luptatum no. Option denique ei vim. Amet magna eripuit sit at.
+          Ius et dicam nominavi, per ea modus ancillae appetere. Ut platonem
+          democritum scripserit mel, ut ius eleifend periculis explicari,
+          summo epicuri honestatis cu pri. Atomorum scriptorem mel no.
+          Pri an tamquam fastidii electram. Ne regione mentitum posidonium ius.
+          Eum dicit offendit id. An vim perpetua forensibus referrentur.
+          Eos omittam ocurreret complectitur in, sed nonumy utroque cu.
+          Te oratio dolorum vocibus qui, ei agam autem has, everti cetero
+          petentium vix te. Vim ne harum facilisis, eum nonumy sapientem te.
+          Eius possit mel cu, dicunt dolorem reprehendunt ea vix, pro diam
+          ludus solet et.
+          Modus diceret lucilius duo cu. Eleifend petentium sed ei,
+          pri graeci consectetuer ut. At vim erat porro.
+          Ut pro ullum instructior.
+          Per no dictas appareat conceptam, dicunt omnium eum et.
+          Pro dicat aliquip et. Altera euripidis forensibus est ne.
+          Alia democritum ius ne, inermis feugait ex mea.
+          Vis ei solum postulant, adhuc paulo id est, unum nihil altera no quo.
+          Erat postea eloquentiam ut eam. Quidam audiam nostrum an vix,
+          ei graece perpetua eos.
         </div>
-        <div title="Business & Economics">
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/food"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/nightlife"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/sports"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/nature"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
+        <div title="Tab 2">
+          <img src="http://lorempixel.com/g/400/500/nightlife" style={{ float: 'left' }}/>
+          Lorem ipsum dolor sit amet, an eam error laboramus conceptam. Eu quiså
+          iudicabit philosophia est. Ex noster phaedrum sapientem sed. Tacimates
+          reformidans vis cu, nec delectus suscipiantur id, sit unum regione
+          luptatum no. Option denique ei vim. Amet magna eripuit sit at.
+          Ius et dicam nominavi, per ea modus ancillae appetere. Ut platonem
+          democritum scripserit mel, ut ius eleifend periculis explicari,
+          summo epicuri honestatis cu pri. Atomorum scriptorem mel no.
+          Pri an tamquam fastidii electram. Ne regione mentitum posidonium ius.
+          Eum dicit offendit id. An vim perpetua forensibus referrentur.
+          Eos omittam ocurreret complectitur in, sed nonumy utroque cu.
+          Te oratio dolorum vocibus qui, ei agam autem has, everti cetero
+          petentium vix te. Vim ne harum facilisis, eum nonumy sapientem te.
+          Eius possit mel cu, dicunt dolorem reprehendunt ea vix, pro diam
+          ludus solet et.
+          Modus diceret lucilius duo cu. Eleifend petentium sed ei,
+          pri graeci consectetuer ut. At vim erat porro.
+          Ut pro ullum instructior.
+          Per no dictas appareat conceptam, dicunt omnium eum et.
+          Pro dicat aliquip et. Altera euripidis forensibus est ne.
+          Alia democritum ius ne, inermis feugait ex mea.
+          Vis ei solum postulant, adhuc paulo id est, unum nihil altera no quo.
+          Erat postea eloquentiam ut eam. Quidam audiam nostrum an vix,
+          ei graece perpetua eos.
         </div>
-        <div title="Science & technology">
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/transport"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/abstract"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/technics"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
-          <a href="#">
-            <figure className="TabView--View--Content">
-              <img src="http://lorempixel.com/g/300/169/cats"/>
-              <figcaption>this is the caption</figcaption>
-            </figure>
-          </a>
+        <div title="Tab 3">
+          Lorem ipsum dolor sit amet, an eam error laboramus conceptam. Eu quiså
+          iudicabit philosophia est. Ex noster phaedrum sapientem sed. Tacimates
+           reformidans vis cu, nec delectus suscipiantur id, sit unum regione
+           luptatum no. Option denique ei vim. Amet magna eripuit sit at.
+           Ius et dicam nominavi, per ea modus ancillae appetere. Ut platonem
+           democritum scripserit mel, ut ius eleifend periculis explicari,
+           summo epicuri honestatis cu pri. Atomorum scriptorem mel no.
+           Pri an tamquam fastidii electram. Ne regione mentitum posidonium ius.
+           Eum dicit offendit id. An vim perpetua forensibus referrentur.
+           Eos omittam ocurreret complectitur in, sed nonumy utroque cu.
+           Te oratio dolorum vocibus qui, ei agam autem has, everti cetero
+           petentium vix te. Vim ne harum facilisis, eum nonumy sapientem te.
+           Eius possit mel cu, dicunt dolorem reprehendunt ea vix, pro diam
+           ludus solet et.
+           Modus diceret lucilius duo cu. Eleifend petentium sed ei,
+           pri graeci consectetuer ut. At vim erat porro.
+           Ut pro ullum instructior.
+           Per no dictas appareat conceptam, dicunt omnium eum et.
+           Pro dicat aliquip et. Altera euripidis forensibus est ne.
+           Alia democritum ius ne, inermis feugait ex mea.
+           Vis ei solum postulant, adhuc paulo id est, unum nihil altera no quo.
+           Erat postea eloquentiam ut eam. Quidam audiam nostrum an vix,
+           ei graece perpetua eos.
         </div>
         <div title="History">
           <a href="#">
--- example.js
+++ example.js
@@ -23,183 +23,20 @@ exports['default'] = _react2['default'].createElement(
       null,
       _react2['default'].createElement(
         'div',
-        { title: 'Politics' },
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/cats' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/people' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/city' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/fashion' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        )
+        { title: 'Tab 1' },
+        _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/400/500/nightlife', style: { float: 'left' } }),
+        'Lorem ipsum dolor sit amet, an eam error laboramus conceptam. Eu quiså iudicabit philosophia est. Ex noster phaedrum sapientem sed. Tacimates reformidans vis cu, nec delectus suscipiantur id, sit unum regione luptatum no. Option denique ei vim. Amet magna eripuit sit at. Ius et dicam nominavi, per ea modus ancillae appetere. Ut platonem democritum scripserit mel, ut ius eleifend periculis explicari, summo epicuri honestatis cu pri. Atomorum scriptorem mel no. Pri an tamquam fastidii electram. Ne regione mentitum posidonium ius. Eum dicit offendit id. An vim perpetua forensibus referrentur. Eos omittam ocurreret complectitur in, sed nonumy utroque cu. Te oratio dolorum vocibus qui, ei agam autem has, everti cetero petentium vix te. Vim ne harum facilisis, eum nonumy sapientem te. Eius possit mel cu, dicunt dolorem reprehendunt ea vix, pro diam ludus solet et. Modus diceret lucilius duo cu. Eleifend petentium sed ei, pri graeci consectetuer ut. At vim erat porro. Ut pro ullum instructior. Per no dictas appareat conceptam, dicunt omnium eum et. Pro dicat aliquip et. Altera euripidis forensibus est ne. Alia democritum ius ne, inermis feugait ex mea. Vis ei solum postulant, adhuc paulo id est, unum nihil altera no quo. Erat postea eloquentiam ut eam. Quidam audiam nostrum an vix, ei graece perpetua eos.'
       ),
       _react2['default'].createElement(
         'div',
-        { title: 'Business & Economics' },
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/food' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nightlife' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/sports' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/nature' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        )
+        { title: 'Tab 2' },
+        _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/400/500/nightlife', style: { float: 'left' } }),
+        'Lorem ipsum dolor sit amet, an eam error laboramus conceptam. Eu quiså iudicabit philosophia est. Ex noster phaedrum sapientem sed. Tacimates reformidans vis cu, nec delectus suscipiantur id, sit unum regione luptatum no. Option denique ei vim. Amet magna eripuit sit at. Ius et dicam nominavi, per ea modus ancillae appetere. Ut platonem democritum scripserit mel, ut ius eleifend periculis explicari, summo epicuri honestatis cu pri. Atomorum scriptorem mel no. Pri an tamquam fastidii electram. Ne regione mentitum posidonium ius. Eum dicit offendit id. An vim perpetua forensibus referrentur. Eos omittam ocurreret complectitur in, sed nonumy utroque cu. Te oratio dolorum vocibus qui, ei agam autem has, everti cetero petentium vix te. Vim ne harum facilisis, eum nonumy sapientem te. Eius possit mel cu, dicunt dolorem reprehendunt ea vix, pro diam ludus solet et. Modus diceret lucilius duo cu. Eleifend petentium sed ei, pri graeci consectetuer ut. At vim erat porro. Ut pro ullum instructior. Per no dictas appareat conceptam, dicunt omnium eum et. Pro dicat aliquip et. Altera euripidis forensibus est ne. Alia democritum ius ne, inermis feugait ex mea. Vis ei solum postulant, adhuc paulo id est, unum nihil altera no quo. Erat postea eloquentiam ut eam. Quidam audiam nostrum an vix, ei graece perpetua eos.'
       ),
       _react2['default'].createElement(
         'div',
-        { title: 'Science & technology' },
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/transport' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/abstract' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/technics' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        ),
-        _react2['default'].createElement(
-          'a',
-          { href: '#' },
-          _react2['default'].createElement(
-            'figure',
-            { className: 'TabView--View--Content' },
-            _react2['default'].createElement('img', { src: 'http://lorempixel.com/g/300/169/cats' }),
-            _react2['default'].createElement(
-              'figcaption',
-              null,
-              'this is the caption'
-            )
-          )
-        )
+        { title: 'Tab 3' },
+        'Lorem ipsum dolor sit amet, an eam error laboramus conceptam. Eu quiså iudicabit philosophia est. Ex noster phaedrum sapientem sed. Tacimates reformidans vis cu, nec delectus suscipiantur id, sit unum regione luptatum no. Option denique ei vim. Amet magna eripuit sit at. Ius et dicam nominavi, per ea modus ancillae appetere. Ut platonem democritum scripserit mel, ut ius eleifend periculis explicari, summo epicuri honestatis cu pri. Atomorum scriptorem mel no. Pri an tamquam fastidii electram. Ne regione mentitum posidonium ius. Eum dicit offendit id. An vim perpetua forensibus referrentur. Eos omittam ocurreret complectitur in, sed nonumy utroque cu. Te oratio dolorum vocibus qui, ei agam autem has, everti cetero petentium vix te. Vim ne harum facilisis, eum nonumy sapientem te. Eius possit mel cu, dicunt dolorem reprehendunt ea vix, pro diam ludus solet et. Modus diceret lucilius duo cu. Eleifend petentium sed ei, pri graeci consectetuer ut. At vim erat porro. Ut pro ullum instructior. Per no dictas appareat conceptam, dicunt omnium eum et. Pro dicat aliquip et. Altera euripidis forensibus est ne. Alia democritum ius ne, inermis feugait ex mea. Vis ei solum postulant, adhuc paulo id est, unum nihil altera no quo. Erat postea eloquentiam ut eam. Quidam audiam nostrum an vix, ei graece perpetua eos.'
       ),
       _react2['default'].createElement(
         'div',
